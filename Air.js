06257f29189c6a8f0354522c70d5e8c79bd1b7c6
knretka <kaylene.retka@gmail.com>
2020-07-10 21:39:38 -0500

    updating repo

---
+++ Air.js
@@ -0,0 +1,18 @@
+fetch('https://aqs.epa.gov/data/api/annualData/byState?email=kaylene.retka@gmail.com&key=sandcrane63&param=45201&bdate=20190101&edate=20191231&state=01')
+  .then(response => response.json())
+  .then(data => console.log(data));
+fetch('https://aqs.epa.gov/data/api/annualData/byState?email=kaylene.retka@gmail.com&key=sandcrane63&param=45201&bdate=20190101&edate=20191231&state=02')
+  .then(response => response.json())
+  .then(data => console.log(data));
+fetch('https://aqs.epa.gov/data/api/annualData/byState?email=kaylene.retka@gmail.com&key=sandcrane63&param=45201&bdate=20190101&edate=20191231&state=03')
+  .then(response => response.json())
+  .then(data => console.log(data));
+fetch('https://aqs.epa.gov/data/api/annualData/byState?email=kaylene.retka@gmail.com&key=sandcrane63&param=45201&bdate=20190101&edate=20191231&state=04')
+  .then(response => response.json())
+  .then(data => console.log(data));
+fetch('https://aqs.epa.gov/data/api/annualData/byState?email=kaylene.retka@gmail.com&key=sandcrane63&param=45201&bdate=20190101&edate=20191231&state=05')
+  .then(response => response.json())
+  .then(data => console.log(data));
+fetch('https://aqs.epa.gov/data/api/annualData/byState?email=kaylene.retka@gmail.com&key=sandcrane63&param=45201&bdate=20190101&edate=20191231&state=06')
+  .then(response => response.json())
+  .then(data => console.log(data));
